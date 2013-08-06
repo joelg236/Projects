f9cfda22f1a4e2322fcd3a9e73d570d465bfab4d
Joel David Gallant <joelgallant236@gmail.com>
2013-08-05 20:09:09 -0600

    Did next prime project

---
+++ Numbers/nextprime.py
@@ -0,0 +1,25 @@
+def factors(o):
+    a = []
+    for x in range(1, o + 1):
+        if o % x == 0:
+            a.append(x)
+    return a
+
+def is_prime(x):
+    return len(factors(x)) == 2
+
+def next_prime(x):
+    while True:
+        x = x + 1
+        if is_prime(x):
+            return x
+
+print('press enter for next')
+
+x = raw_input()
+i = 0
+
+while(x == ''):
+    i = next_prime(i)
+    print(i)
+    x = raw_input()
