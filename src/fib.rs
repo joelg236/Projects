175f00ccaeb8bb80b16b308be9e46c9c078d2d6b
Joel Gallant <joel@joelgallant.me>
2016-09-13 15:41:09 -0600

    Fibonacci test

--- src/fib.rs
+++ src/fib.rs
@@ -18,4 +18,12 @@ fn fibonacci(n: usize) -> Vec<i64> {
 
 #[test]
 fn test() {
+    assert_eq!(fibonacci(0), vec![0;0]);
+    assert_eq!(fibonacci(1), vec![1]);
+    assert_eq!(fibonacci(2), vec![1, 2]);
+    assert_eq!(fibonacci(3), vec![1, 2, 3]);
+    assert_eq!(fibonacci(4), vec![1, 2, 3, 5]);
+    assert_eq!(fibonacci(5), vec![1, 2, 3, 5, 8]);
+    assert_eq!(fibonacci(6), vec![1, 2, 3, 5, 8, 13]);
+    assert_eq!(fibonacci(7), vec![1, 2, 3, 5, 8, 13, 21]);
 }
